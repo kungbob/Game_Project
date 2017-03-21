bce15a4168f93c3bd19187cc903eb362d53968ad
kungbob <kung.bob@gmail.com>
2017-03-21 19:50:13 +0800

    All things included.

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public static int Money;
+    public int startMoney = 400;
+    public static float IncreaseRate = 1f;
+    private float Countdown = 0f;
+
+    // Use this for initialization
+    void Start () {
+        Money = startMoney;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        if (Countdown <= 0)
+        {
+            Money += 1;
+            Countdown = 1f / IncreaseRate;
+        }
+
+        Countdown -= Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    [Header("Optional")]
+    public GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    BuildManager buildManager;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+
+        buildManager = BuildManager.instance;
+    }
+
+    public Vector3 GetBuildPosition ()
+    {
+        return transform.position + positionOffset;
+    }
+
+    void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (!buildManager.CanBuild)
+            return;
+
+        if (turret != null)
+        {
+            Debug.Log("Can't build there! - TODO: Display on screen.");
+            return;
+        }
+
+        buildManager.BuildTurretOn(this);
+       
+    }
+
+    void OnMouseEnter ()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (!buildManager.CanBuild)
+            return;
+
+
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+  
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
