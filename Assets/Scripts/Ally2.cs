b6c4716626281427370d0f3638a6ee09a23203e2
kungbob <kung.bob@gmail.com>
2017-04-24 09:41:57 +0800

    Pre-alpha version

---
+++ Assets/Scripts/Ally2.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ally : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    public int health = 100;
+
+    public int value = 50;
+
+    public GameObject deathEffect;
+
+    private Transform target;
+    private Transform aim;
+
+    private int wavepointIndex = 0;
+
+    [Header("Attributes")]
+
+    public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
+
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = AllyWaypoints.points[0];
+        InvokeRepeating("UpdateTarget", 0f, 0.05f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            aim = nearestEnemy.transform;
+        }
+        else
+        {
+            aim = null;
+        }
+    }
+
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aim == null)
+        {
+            Vector3 dir = target.position - transform.position;
+            transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+            if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+            {
+                GetNextWaypoint();
+            }
+        }
+        else
+        {
+            Vector3 dir = aim.position - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+            partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+            if (fireCountdown <= 0)
+            {
+                Shoot();
+                fireCountdown = 1f / fireRate;
+            }
+
+            fireCountdown -= Time.deltaTime;
+        }
+    }
+
+    void Shoot ()
+    {
+        GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGo.GetComponent<Bullet>();
+
+        if (bullet != null)
+            bullet.Seek(aim);
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= AllyWaypoints.points.Length - 1)
+        {
+            EndPath();
+            return;
+        }
+        wavepointIndex++;
+        target = AllyWaypoints.points[wavepointIndex];
+    }
+
+    void EndPath()
+    {
+        
+        Destroy(gameObject);
+    }
+}
